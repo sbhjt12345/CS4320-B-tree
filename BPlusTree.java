82bd2912cef3c821ddbc063ae05c39aa9745edff
Jiantao Hu <jh2468@cornell.edu>
2015-10-01 14:12:04 -0400

    BPlusTree

---
+++ BPlusTree.java
@@ -0,0 +1,131 @@
+import java.util.AbstractMap;
+import java.util.Map.Entry;
+
+/**
+ * BPlusTree Class Assumptions: 1. No duplicate keys inserted 2. Order D:
+ * D<=number of keys in a node <=2*D 3. All keys are non-negative
+ * TODO: Rename to BPlusTree
+ */
+public class BPlusTree<K extends Comparable<K>, T> {
+
+	public Node<K,T> root;
+	public static final int D = 2;
+
+	/**
+	 * TODO Search the value for a specific key
+	 * 
+	 * @param key
+	 * @return value
+	 */
+	public T search(K key) {
+		return search_helper(root,key);
+	}
+	
+	public T search_helper(Node<K,T> root, K key){
+		if (root.isLeafNode==true) {
+			LeafNode<K,T> tmp = (LeafNode<K,T>) root;
+			int i = 0;
+			while (i<tmp.keys.size()){
+				if (key==tmp.keys.get(i)) return tmp.values.get(i);
+				i++;
+			}
+		}
+		else{
+			IndexNode<K,T> tmp2 = (IndexNode<K,T>) root;
+			if (key.compareTo(tmp2.keys.get(0)) < 0){
+				return search_helper((Node<K,T>)tmp2.children.get(0),key);
+			}
+			else if (key.compareTo(tmp2.keys.get(tmp2.keys.size()-1))>0){
+				return search_helper((Node<K,T>)tmp2.children.get(tmp2.children.size()-1),key);
+			}
+			else{
+				int i = 0;
+				while (i<tmp2.keys.size()){
+					if (key.compareTo(tmp2.keys.get(i))>=0 && key.compareTo(tmp2.keys.get(i+1))<0){
+						return search_helper((Node<K,T>)tmp2.children.get(i+1),key);
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * TODO Insert a key/value pair into the BPlusTree
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void insert(K key, T value) {
+
+	}
+
+	/**
+	 * TODO Split a leaf node and return the new right node and the splitting
+	 * key as an Entry<slitingKey, RightNode>
+	 * 
+	 * @param leaf, any other relevant data
+	 * @return the key/node pair as an Entry
+	 */
+	public Entry<K, Node<K,T>> splitLeafNode(LeafNode<K,T> leaf, ...) {
+
+		return null;
+	}
+
+	/**
+	 * TODO split an indexNode and return the new right node and the splitting
+	 * key as an Entry<slitingKey, RightNode>
+	 * 
+	 * @param index, any other relevant data
+	 * @return new key/node pair as an Entry
+	 */
+	public Entry<K, Node<K,T>> splitIndexNode(IndexNode<K,T> index, ...) {
+
+		return null;
+	}
+
+	/**
+	 * TODO Delete a key/value pair from this B+Tree
+	 * 
+	 * @param key
+	 */
+	public void delete(K key) {
+
+	}
+
+	/**
+	 * TODO Handle LeafNode Underflow (merge or redistribution)
+	 * 
+	 * @param left
+	 *            : the smaller node
+	 * @param right
+	 *            : the bigger node
+	 * @param parent
+	 *            : their parent index node
+	 * @return the splitkey position in parent if merged so that parent can
+	 *         delete the splitkey later on. -1 otherwise
+	 */
+	public int handleLeafNodeUnderflow(LeafNode<K,T> left, LeafNode<K,T> right,
+			IndexNode<K,T> parent) {
+		return -1;
+
+	}
+
+	/**
+	 * TODO Handle IndexNode Underflow (merge or redistribution)
+	 * 
+	 * @param left
+	 *            : the smaller node
+	 * @param right
+	 *            : the bigger node
+	 * @param parent
+	 *            : their parent index node
+	 * @return the splitkey position in parent if merged so that parent can
+	 *         delete the splitkey later on. -1 otherwise
+	 */
+	public int handleIndexNodeUnderflow(IndexNode<K,T> leftIndex,
+			IndexNode<K,T> rightIndex, IndexNode<K,T> parent) {
+		return -1;
+	}
+
+}
